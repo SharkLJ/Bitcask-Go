8997a3b717924192240c6f917c3b735548152c89
LJ <1250343067@qq.com>
2024-01-04 23:33:33 +0800

    整个项目

---
+++ index/btree_test.go
@@ -0,0 +1,52 @@
+// @Author LJ 2024-01-04 22:53:00
+package index
+
+import (
+	"bitcask/data"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestBTree_Put(t *testing.T) {
+	bTree := NewBTree()
+
+	rep1 := bTree.Put(nil, &data.LogRecordPos{Fid: 1, Offset: 10})
+	assert.True(t, rep1)
+
+	rep2 := bTree.Put([]byte("a"), &data.LogRecordPos{Fid: 1, Offset: 20})
+	assert.True(t, rep2)
+}
+
+func TestBTree_Get(t *testing.T) {
+	bTree := NewBTree()
+
+	rep1 := bTree.Put(nil, &data.LogRecordPos{Fid: 1, Offset: 10})
+	assert.True(t, rep1)
+
+	pos1 := bTree.Get(nil)
+	assert.Equal(t, uint32(1),pos1.Fid)
+	assert.Equal(t, int64(10),pos1.Offset)
+
+	rep2 := bTree.Put([]byte("a"), &data.LogRecordPos{Fid: 1, Offset: 20})
+	assert.True(t, rep2)
+	rep3 := bTree.Put([]byte("a"), &data.LogRecordPos{Fid: 1, Offset: 21})
+	assert.True(t, rep3)
+
+	pos2 := bTree.Get([]byte("a"))
+	assert.Equal(t, uint32(1), pos2.Fid)
+	assert.Equal(t, int64(21), pos2.Offset)
+}
+
+func TestBTree_Delete(t *testing.T) {
+	bTree := NewBTree()
+
+	rep1 := bTree.Put(nil, &data.LogRecordPos{Fid: 1, Offset: 10})
+	assert.True(t, rep1)
+
+	   //rep2 := bTree.Delete(nil)
+	   //assert.True(t, rep2)
+
+	pos1 := bTree.Get(nil)
+	t.Log(pos1)
+
+}
